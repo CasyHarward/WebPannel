80138c31b2a745b0f3169fc5c78d5b2cef43da5e
CodeEXtremer <94539452+CodeExtremer@users.noreply.github.com>
2025-01-14 12:35:48 +0500

    Create db.js

---
+++ config/db.js
@@ -0,0 +1,6 @@
+// Simulating a database with a hardcoded user for demonstration purposes
+const users = [
+    { id: 1, username: 'john_doe', password: '$2a$10$g59j.V1NnG7VsZGzT4Oq9e.J.j3Svk.FnY.nPORekpG3cEeqpeCQW' } // password: 123456
+];
+
+module.exports = { users };
